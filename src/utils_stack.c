ead414cf9f2da2d994f5a54bb4f17e15cf67f040
Merche_Linux_Asus <mergarciam@gmail.com>
2025-05-20 20:47:51 +0200

    20250520: adding stack parsinggit add src/*.c

--- src/utils_stack.c
+++ src/utils_stack.c
@@ -6,7 +6,7 @@
 /*   By: mergarci <mergarci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 18:13:37 by mergarci          #+#    #+#             */
-/*   Updated: 2025/05/19 19:56:08 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:51:42 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ t_PS_list	*PS_lstnew(int new_element)
 	if (!list)
 		return (NULL);
 	list->content = new_element;
-	ft_printf("PS_lstnew: hemos creado: %d\n", list->content);
 	list->index = 0;
 	list->next = NULL;
 	return (list);
@@ -32,7 +31,6 @@ int	PS_lstsize(t_PS_list *lst)
 	t_PS_list	*aux;
 
 	cont = 0;
-	ft_printf("PS_lst_size: %d\n", lst->content);
 	if (lst != NULL)
 	{
 		aux = lst;
@@ -51,15 +49,58 @@ void	PS_lstadd_back(t_PS_list **lst, t_PS_list *lnew)
 
 	if (*lst == NULL)
 	{
-		ft_printf("PS_lstadd_back: vamos a guardar: %d, %p, lnew: %p\n", lnew->content , *lst, *lnew);
+		//ft_printf("PS_lstadd_back: vamos a guardar: %d, %p, lnew: %p\n", lnew->content , *lst, *lnew);
 		*lst = lnew;
 		return ;
 	}
 	aux = *lst;
 	while (aux->next != NULL)
 	{
-		ft_printf("__recorriendo hasta llegar al final: %d\n", lnew->content);
+		//ft_printf("__recorriendo hasta llegar al final: %d\n", lnew->content);
 		aux = aux->next;
 	}
 	aux->next = lnew;
+}
+
+void PS_print_content(t_PS_list *stack)
+{
+	t_PS_list *aux;
+	
+	aux = stack;
+	ft_printf(">>>>IMPRIMIENDO<<<<\n");
+	while (aux)
+	{
+		ft_printf("%d_", aux->content);
+		aux = aux->next;
+	}
+	ft_printf("\n");
+}
+
+int check_duplicates(int number, t_PS_list **stack)
+{
+	t_PS_list	*aux;
+
+	aux = *stack;
+	while (aux)
+	{
+		if (number == aux->content)
+			return (EXIT_FAILURE);
+		aux = aux->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+void	PS_lstclear(t_PS_list **stack)
+{
+	t_PS_list	*aux;
+
+	if (stack)
+	{
+		while (*stack != NULL)
+		{
+			aux = (*stack)->next;
+			free(*stack);
+			*stack = aux;
+		}
+	}
 }
